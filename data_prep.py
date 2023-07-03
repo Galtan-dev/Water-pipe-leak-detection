3187bb36528d87319f7402ab9b29fe79974ba72c
Galtan-dev <jakub.seiner@seznam.cz>
2023-07-03 17:32:11 +0200

    Data preparation and first detection

---
+++ data_prep.py
@@ -0,0 +1,67 @@
+from scipy.io import wavfile
+import numpy as np
+import math
+
+# 37s full open = 1 776 000 values
+
+
+
+# open file with sound data
+sampling_frequency, sound_data_orig = wavfile.read('video_and_sound.wav')
+start = 1790000
+end = 1800000
+sound_data = []
+
+for i in range(0, len(sound_data_orig)):
+    if sound_data_orig[i]==0:
+        sound_data.append(sound_data_orig[i])
+    else:
+        sound_data.append(math.log(abs(sound_data_orig[i])))
+
+
+# inputs one by one - continuously drifted by one
+in_1 = []
+in_2 = []
+in_3 = []
+in_4 = []
+in_5 = []
+target = []
+for i in range(start, end + 1):
+    in_1.append(sound_data[i])
+for i in range(start + 1, end + 2):
+    in_2.append(sound_data[i])
+for i in range(start + 2, end + 3):
+    in_3.append(sound_data[i])
+for i in range(start + 3, end + 4):
+    in_4.append(sound_data[i])
+for i in range(start + 4, end + 5):
+    in_5.append(sound_data[i])
+for i in range(start + 5, end + 6):
+    target.append(sound_data[i])
+
+# creation of matrix of inputs and target vector
+k = len(in_1)
+in_matrix = []
+target_matrix = []
+for i in range(0, k):
+    in_matrix.append(in_1[i])
+for i in range(0, k):
+    in_matrix.append(in_2[i])
+for i in range(0, k):
+    in_matrix.append(in_3[i])
+for i in range(0, k):
+    in_matrix.append(in_4[i])
+for i in range(0, k):
+    in_matrix.append(in_5[i])
+for i in range(0, k):
+    target_matrix.append(target[i])
+
+in_matrix = np.asarray(in_matrix)
+in_matrix = np.reshape(in_matrix, [k, 5], order="F")
+target_vector = np.reshape(target_matrix, (k,))
+
+np.savetxt("in_sound_data.csv", in_matrix, delimiter=",")
+np.savetxt("target_sound_data.csv", target_vector, delimiter=",")
+
+
+
--- leak_detection_main.py
+++ leak_detection_main.py
@@ -1,61 +1,43 @@
-from scipy.io import wavfile
-import soundfile as sf
-from scipy import signal
 import numpy as np
 import matplotlib.pylab as plt
 import padasip as pa
-import csv
-import pandas as pd
 
-# open file with sound data
-sampling_frequency, sound_data = wavfile.read('video_and_sound.wav')
+# open data
+with open("in_sound_data.csv", "r", encoding="utf-8") as in_sound_data:
+    in_matrix = (np.genfromtxt(in_sound_data, delimiter=",", skip_header=0))
 
-# inputs one by one - continuously drifted by one
-in_1 = []
-in_2 = []
-in_3 = []
-in_4 = []
-in_5 = []
-target = []
-for i in range(0, 8565114):
-    in_1.append(sound_data[i])
-for i in range(1, 8565115):
-    in_2.append(sound_data[i])
-for i in range(2, 8565116):
-    in_3.append(sound_data[i])
-for i in range(3, 8565117):
-    in_4.append(sound_data[i])
-for i in range(4, 8565118):
-    in_5.append(sound_data[i])
-for i in range(5, 8565119):
-    target.append(sound_data[i])
-
-# creation of matrix of inputs and target vector
-in_matrix = []
-target_matrix = []
-for i in range(0, 8565114):
-    in_matrix.append(in_1[i])
-for i in range(0, 8565114):
-    in_matrix.append(in_2[i])
-for i in range(0, 8565114):
-    in_matrix.append(in_3[i])
-for i in range(0, 8565114):
-    in_matrix.append(in_4[i])
-for i in range(0, 8565114):
-    in_matrix.append(in_5[i])
-for i in range(0, 8565114):
-    target_matrix.append(target[i])
-
-in_matrix = np.asarray(in_matrix)
-in_matrix = np.reshape(in_matrix, [8565114, 5], order="F")
-
-d = np.reshape(target_matrix, (8565114,))
-x = np.reshape(in_matrix, (8565114, 5))
+with open("target_sound_data.csv", "r", encoding="utf-8") as target_sound_data:
+    target_matrix = (np.genfromtxt(target_sound_data, delimiter=",", skip_header=0))
+k = len(target_matrix)
+d = np.reshape(target_matrix, (k,))
+x = np.reshape(in_matrix, (k, 5))
 
 # detection
-# f = pa.filters.FilterGNGD(n=1, mu=0.40, w="zeros")
+# f = pa.filters.FilterGNGD(n=5, mu=0.50, w="zeros")
 f = pa.filters.FilterAP(n=5, order=5, mu=0.50, ifc=0.005, w="zeros")
 y, e, w = f.run(d, x)
 
-le = pa.detection.learning_entropy(w, m=50, order=1)
-# le = pa.detection.ELBND(E, w, function="max")
+# det = pa.detection.learning_entropy(w, m=50, order=1, alpha=[8., 9., 10., 11., 12., 13.])
+# det = pa.detection.ELBND(w, e, function="max")
+det = pa.detection.ESE(w)
+
+# ploting
+fig, axs = plt.subplots(2, 1)
+axs[0].plot(d)
+axs[1].plot(det)
+fig.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
