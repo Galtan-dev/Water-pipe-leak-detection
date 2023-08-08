d8865ef9177d3489291f5a919006952998091c9d
Galtan-dev <jakub.seiner@seznam.cz>
2023-08-08 10:57:53 +0200

    new records and results

--- data_prep.py
+++ data_prep.py
@@ -3,9 +3,13 @@ import numpy as np
 import math
 
 # open file with sound data
-sampling_frequency, sound_data_orig = wavfile.read('video_and_sound.wav')
-start = 5600000
-end = 5700000
+sampling_frequency, sound_data_orig = wavfile.read("record_2.wav")
+
+
+
+filter_length = 10   # vždy se udělá o jeden méně ve výsledku, počítat s tím u nastavování filtru při detekci, nebude 50 vstupů ale jen 49
+start = 220000
+end = 280000
 sound_data = []
 
 for i in range(0, len(sound_data_orig)):
@@ -14,49 +18,74 @@ for i in range(0, len(sound_data_orig)):
     else:
         sound_data.append(math.log(abs(sound_data_orig[i])))
 
-# inputs one by one - continuously drifted by one
-in_1 = []
-in_2 = []
-in_3 = []
-in_4 = []
-in_5 = []
-target = []
-for i in range(start, end + 1):
-    in_1.append(sound_data[i])
-for i in range(start + 1, end + 2):
-    in_2.append(sound_data[i])
-for i in range(start + 2, end + 3):
-    in_3.append(sound_data[i])
-for i in range(start + 3, end + 4):
-    in_4.append(sound_data[i])
-for i in range(start + 4, end + 5):
-    in_5.append(sound_data[i])
-for i in range(start + 5, end + 6):
-    target.append(sound_data[i])
-
-# creation of matrix of inputs and target vector
-k = len(in_1)
 in_matrix = []
 target_matrix = []
-for i in range(0, k):
-    in_matrix.append(in_1[i])
-for i in range(0, k):
-    in_matrix.append(in_2[i])
-for i in range(0, k):
-    in_matrix.append(in_3[i])
-for i in range(0, k):
-    in_matrix.append(in_4[i])
-for i in range(0, k):
-    in_matrix.append(in_5[i])
-for i in range(0, k):
+
+# separation of inputs
+for i in range(1, filter_length):
+    name = "in_" + str(i)
+    name = []
+    for j in range((start + (i - 1)), end + i):
+        name.append(sound_data[j])
+    for k in range(0, len(name)):
+        in_matrix.append(name[k])
+
+target = []
+for i in range(start + (filter_length - 1), end + filter_length):
+    target.append(sound_data[i])
+for i in range(0, len(target)):
     target_matrix.append(target[i])
 
+m = len(name)
 in_matrix = np.asarray(in_matrix)
-in_matrix = np.reshape(in_matrix, [k, 5], order="F")
-target_vector = np.reshape(target_matrix, (k,))
+in_matrix = np.reshape(in_matrix, [m, filter_length - 1], order="F")
+target_vector = np.reshape(target_matrix, (m,))
 
 np.savetxt("in_sound_data.csv", in_matrix, delimiter=",")
 np.savetxt("target_sound_data.csv", target_vector, delimiter=",")
 
 
 
+
+
+
+
+
+
+
+# # inputs one by one - continuously drifted by one
+# in_1 = []
+# in_2 = []
+# in_3 = []
+# in_4 = []
+# in_5 = []
+# target = []
+# for i in range(start, end + 1):
+#     in_1.append(sound_data[i])
+# for i in range(start + 1, end + 2):
+#     in_2.append(sound_data[i])
+# for i in range(start + 2, end + 3):
+#     in_3.append(sound_data[i])
+# for i in range(start + 3, end + 4):
+#     in_4.append(sound_data[i])
+# for i in range(start + 4, end + 5):
+#     in_5.append(sound_data[i])
+# for i in range(start + 5, end + 6):
+#     target.append(sound_data[i])
+
+# # creation of matrix of inputs and target vector
+# k = len(in_1)
+# in_matrix = []
+# target_matrix = []
+# for i in range(0, k):
+#     in_matrix.append(in_1[i])
+# for i in range(0, k):
+#     in_matrix.append(in_2[i])
+# for i in range(0, k):
+#     in_matrix.append(in_3[i])
+# for i in range(0, k):
+#     in_matrix.append(in_4[i])
+# for i in range(0, k):
+#     in_matrix.append(in_5[i])
+# for i in range(0, k):
+#     target_matrix.append(target[i])
--- leak_detection_main.py
+++ leak_detection_main.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pylab as plt
 import padasip as pa
 
+length = 9
+
 # open data
 with open("in_sound_data.csv", "r", encoding="utf-8") as in_sound_data:
     in_matrix = (np.genfromtxt(in_sound_data, delimiter=",", skip_header=0))
@@ -10,16 +12,20 @@ with open("target_sound_data.csv", "r", encoding="utf-8") as target_sound_data:
     target_matrix = (np.genfromtxt(target_sound_data, delimiter=",", skip_header=0))
 k = len(target_matrix)
 d = np.reshape(target_matrix, (k,))
-x = np.reshape(in_matrix, (k, 5))
+x = np.reshape(in_matrix, (k, length))
+
+print("midline")
 
 # detection
 # f = pa.filters.FilterGNGD(n=5, mu=0.50, w="zeros")
-f = pa.filters.FilterAP(n=5, order=5, mu=0.40, ifc=0.005, w="zeros")
+f = pa.filters.FilterAP(n=length, order=3, mu=0.50, ifc=0.005, w="zeros")
 y, e, w = f.run(d, x)
 
-# det = pa.detection.learning_entropy(w, m=100, order=3, alpha=[8., 9., 10., 11., 12., 13.])
-det = pa.detection.ELBND(w, e, function="sum")
-# det = pa.detection.ESE(w)
+print("rex")
+
+# det = pa.detection.learning_entropy(w, m=150, order=2, alpha=[8., 9., 10., 11., 12., 13.])
+# det = pa.detection.ELBND(w, e, function="sum")
+det = pa.detection.ESE(w)
 
 # ploting
 fig, axs = plt.subplots(2, 1)
